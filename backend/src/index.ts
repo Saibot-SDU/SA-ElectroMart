a46eedc75561fdf60be339b54bb78aee09bd7103
Tobias Kristensen <tobiaskristensen12@gmail.com>
2024-02-24 12:15:59 +0100

    Add product models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from 'express';
+import { PlayStation } from './models/PlayStation';
+import { Laptop } from './models/Laptop';
+import { Phone } from './models/Phone';
+import { SSD } from './models/SSD';
 
 const app = express();
 const port = 3000;
+
+const recommendedProducts: (Laptop | Phone | SSD | PlayStation)[] = [
+    
+];
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
---
+++ backend/src/models/Phone.ts
@@ -0,0 +1,17 @@
+export class Phone {
+    private title: string;
+    private imageUrl: string;
+    private basePrice: number;
+    private taxRate: number = 0.25;
+    private discountRate: number = 0.15;
+
+    constructor(title: string, imageUrl: string, basePrice: number) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.basePrice = basePrice;
+    }
+
+    public getPrice(): number {
+        return (this.basePrice * (1 - this.discountRate)) * this.taxRate;
+    }
+}
